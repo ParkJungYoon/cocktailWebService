25062690c095e16c96fd01ba9e1b265fdbf5f5e3
sj_kim <ksj961223@gmail.com>
2022-04-21 14:20:29 +0900

    [Modify] 파일 수정

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -60,6 +60,13 @@ function Portfolio() {
             isEditable={portfolioOwner.id === userState.user?.id}
           />
         </Col>
+        <Col>
+
+          <div style={{ textAlign: "center" }}>
+            학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
+          </div>
+
+        </Col>
       </Row>
     </Container>
   );
