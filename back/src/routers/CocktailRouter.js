4dcd1da720a065b33a372ef6a6d97ebf4e81dbe1
parkjungyoon <tbr06057@naver.com>
2022-04-21 17:25:28 +0900

    [Modify] cocktail router

--- back/src/routers/CocktailRouter.js
+++ back/src/routers/CocktailRouter.js
@@ -16,9 +16,9 @@ CocktailRouter.post("/addRank", async (req, res, next) => {
 
 CocktailRouter.post("/addCocktail", async (req, res, next) => {
   try {
-    const { name, ingredient, rank } = req.body;
-    console.log(name, ingredient, rank);
-    const addCocktail = await CocktailService.addCocktail({ name, ingredient, rank });
+    const { name, ingredient, imageUrl, rank } = req.body;
+    console.log(name, ingredient, imageUrl, rank);
+    const addCocktail = await CocktailService.addCocktail({ name, ingredient, imageUrl, rank });
     res.status(200).json(addCocktail);
   } catch (error) {
     next(error);
@@ -41,7 +41,7 @@ CocktailRouter.post("/findCocktail", async (req, res, next) => {
 CocktailRouter.post("/modifyCocktail", async (req, res, next) => {
   try {
     const { cocktailId, name, ingredient, rank } = req.body;
-    const modifiedCocktail = await CocktailService.modify({ cocktailId, name, ingredient, rank });
+    const modifiedCocktail = await CocktailService.modify({ cocktailId, name, ingredient, imageUrl, rank });
     res.status(200).json(modifiedCocktail);
   } catch (error) {
     next(error);
