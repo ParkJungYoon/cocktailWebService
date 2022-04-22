e91256c47a7f0b099b3d528542a94d952ce6b2cf
parkjungyoon <tbr06057@naver.com>
2022-04-23 08:48:21 +0900

    [Add] create user cocktail service

--- back/src/services/cocktailService.js
+++ back/src/services/cocktailService.js
@@ -1,4 +1,4 @@
-import { CocktailModel } from "../db";
+import { CocktailModel, UserCocktailModel } from "../db";
 
 class CocktailService {
   // db 조회
@@ -12,6 +12,29 @@ class CocktailService {
     }
     return cocktail;
   };
+  // 유저 칵테일 추가
+  static addCocktail = async ({
+    name,
+    ingredient,
+    description,
+    imageUrl,
+    rank,
+  }) => {
+    const addCocktailData = { name, ingredient, imageUrl, user_id };
+    const addCocktail = await UserCocktailModel.addCocktail(addCocktailData);
+    return addCocktail;
+  };
+  // 유저 칵테일 수정
+  static modify = async ({ _id, name, ingredient, imageUrl, user_id }) => {
+    const updateCocktailData = { _id, name, ingredient, imageUrl, user_id };
+    const filter = { _id: _id };
+    const modifiedCocktail = await UserCocktailModel.modify(
+      filter,
+      updateCocktailData
+    );
+    return modifiedCocktail;
+  };
+  // 유저 칵테일 삭제
 }
 
 export { CocktailService };
