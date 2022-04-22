45ee70aa0896a7d3e3d4f7a8c0a35386a4be507c
parkjungyoon <tbr06057@naver.com>
2022-04-22 18:52:47 +0900

    [Create] 칵테일 조회 API

--- back/src/services/CocktailService.js
+++ back/src/services/CocktailService.js
@@ -1,29 +1,16 @@
 import { CocktailModel } from "../db";
-import { RankModel } from "../db";
+
 class CocktailService {
-  static addCocktailRank = async ({ name, visitors, years, rank }) => {
-    const addCocktailRankData = { name, visitors, years, rank };
-    const addCocktail = await RankModel.addCocktail(addCocktailRankData);
-    return addCocktail;
-  };
-  static addCocktail = async ({ name, ingredient, imageUrl, rank }) => {
-    const addCocktailData = { name, ingredient, imageUrl, rank };
-    const addCocktail = await CocktailModel.addCocktail(addCocktailData);
-    return addCocktail;
-  };
-  static findCocktail = async ({ cocktail }) => {
-    const findCocktail = await CocktailModel.findCocktail({ cocktail });
-    if (!findCocktail) {
-      const errorMessage = "해당 칵테일 내역이 없습니다.";
+  // db 조회
+  static getCocktailInfo = async ({ name }) => {
+    console.log(name);
+    const cocktail = await CocktailModel.findCocktail({ name });
+    if (!cocktail) {
+      const errorMessage =
+        "해당 칵테일 내역이 없습니다. 다시 한 번 확인해 주세요.";
       return { errorMessage };
     }
-    return findCocktail;
-  };
-  static modify = async ({ cocktailId, name, ingredient, imageUrl, rank }) => {
-    const updateCocktailData = { name, ingredient, imageUrl, rank };
-    const filter = { _id: cocktailId };
-    const modifiedCocktail = await CocktailModel.modify(filter, updateCocktailData);
-    return modifiedCocktail;
+    return cocktail;
   };
 }
 
