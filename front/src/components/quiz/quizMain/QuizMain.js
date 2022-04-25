ca3af4a54fd7516d466abebac26ac42365fa3a20
ksm9317 <ksm9317@gmail.com>
2022-04-26 00:18:13 +0900

    [Modify] 페이지 배치 변경

--- front/src/components/quiz/quizMain/QuizMain.js
+++ front/src/components/quiz/quizMain/QuizMain.js
@@ -5,42 +5,43 @@ import "./QuizMain.css";
 import QuestionImg from "../questionImg/QuestionImg";
 import defaultImage from "../questionImg/defaultImage.jpg";
 import StandardShaker from "../questionImg/StandardShaker.jpg";
+import { Container, Box, Grid } from "@mui/material";
 
 export default class Quiz extends Component {
   // initiating the local state
   state = {
     quiestions: {
-      1: "술에 술을 섞거나 술에 청량음료나 과즙 음료, 기타 부재료를 이용하여 혼합시킨 것은?",
-      2: "다음 기구의 이름은?",
-      3: "다음 설명으로 틀린 것은?",
-      4: "뜨거운 칵테일은 어떤 것인가?",
-      5: "칵테일 용어 설명 중 틀린 것은?",
+      1: "Q1. 술에 술을 섞거나 술에 청량음료나 과즙 음료, 기타 부재료를 이용하여 혼합시킨 것은?",
+      2: "Q2. 다음 기구의 이름은?",
+      3: "Q3. 다음 설명으로 틀린 것은?",
+      4: "Q4. 뜨거운 칵테일은 어떤 것인가?",
+      5: "Q5. 칵테일 용어 설명 중 틀린 것은?",
     },
     answers: {
       1: {
-        1: "칵테일 ",
-        2: "하드 드링크",
-        3: "소프트 드링크",
+        1: "1. 칵테일 ",
+        2: "2. 하드 드링크",
+        3: "3. 소프트 드링크",
       },
       2: {
-        1: "스탠다드 셰이커(Standard Shaker)",
-        2: "믹싱 글라스(Mixing Glass)",
-        3: "스트레이너(Strainer)",
+        1: "1. 스탠다드 셰이커(Standard Shaker)",
+        2: "2. 믹싱 글라스(Mixing Glass)",
+        3: "3. 스트레이너(Strainer)",
       },
       3: {
-        1: "버진은 논 알코올이 칵테일이다.",
-        2: "막테일은 논 알코올이 칵테일이다.",
-        3: "체이서는 칵테일 가니쉬 중 하나이다.",
+        1: "1. 버진은 논 알코올이 칵테일이다.",
+        2: "2. 막테일은 논 알코올이 칵테일이다.",
+        3: "3. 체이서는 칵테일 가니쉬 중 하나이다.",
       },
       4: {
-        1: "Pink Lady",
-        2: "Irish Coffee",
-        3: "Pina Colada",
+        1: "1. Pink Lady",
+        2: "2. Irish Coffee",
+        3: "3. Pina Colada",
       },
       5: {
-        1: "Stir : 잘 섞이도록 저어 주는 것.",
-        2: "Float : 한가지의 술에 다른 술이 혼합되지 않게 띄우는 것.",
-        3: "Strainer : 과육을 제거하고 껍데기만을 짜 넣는다는 의미.",
+        1: "1. Stir : 잘 섞이도록 저어 주는 것.",
+        2: "2. Float : 한가지의 술에 다른 술이 혼합되지 않게 띄우는 것.",
+        3: "3. Strainer : 과육을 제거하고 껍데기만을 짜 넣는다는 의미.",
         // Strainer → 믹싱글라스에 혼합한 술을 따를 때 얼음이 흘러나오지 않도록 걸러주기 위해 사용하는 도구.
       },
     },
@@ -52,11 +53,11 @@ export default class Quiz extends Component {
       5: "3",
     },
     imgs: {
-      1: defaultImage,
+      1: " ",
       2: StandardShaker,
-      3: defaultImage,
-      4: defaultImage,
-      5: defaultImage,
+      3: " ",
+      4: " ",
+      5: " ",
     },
     correctAnswer: 0,
     clickedAnswer: 0,
@@ -109,17 +110,40 @@ export default class Quiz extends Component {
               style={{
                 display: "flex",
                 justifyContent: "center",
+                marginTop: "2rem",
+                marginBottom: "2rem",
               }}
             >
               <QuestionImg img={imgs[step]} />
             </div>
-            <Answer
-              answer={answers[step]}
-              step={step}
-              checkAnswer={this.checkAnswer}
-              correctAnswer={correctAnswer}
-              clickedAnswer={clickedAnswer}
-            />
+
+            <Grid container mb="5%">
+              <Grid item xs={4} md={4}></Grid>
+              <Grid
+                item
+                xs={4}
+                md={4}
+                //white bar
+                sx={{
+                  backgroundColor: "white",
+                  height: "3px",
+                  borderRadius: "5px",
+                }}
+              ></Grid>
+              <Grid item xs={4} md={4}>
+                {/* 1-3 */}
+              </Grid>
+            </Grid>
+
+            <div>
+              <Answer
+                answer={answers[step]}
+                step={step}
+                checkAnswer={this.checkAnswer}
+                correctAnswer={correctAnswer}
+                clickedAnswer={clickedAnswer}
+              />
+            </div>
             <button
               className="NextStep"
               disabled={
