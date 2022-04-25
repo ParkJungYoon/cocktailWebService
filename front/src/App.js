27cd29f0b9d5faf32e63cfaa9731ac8553742e50
ksm9317 <ksm9317@gmail.com>
2022-04-25 18:23:09 +0900

    [Create] Quiz 기능 초기 설정

--- front/src/App.js
+++ front/src/App.js
@@ -11,6 +11,10 @@ import Fullpage from "./components/Landingpage/Fullpage";
 import Introduce from "./components/introduce/Introduce";
 import Mypage from "./components/mypage/Mypage";
 import Bookmark from "./components/mypage/Bookmark";
+import Like from "./components/mypage/Like";
+import UserInfo from "./components/mypage/UserInfo";
+import UserTab from "./components/mypage/UserTab";
+import QuizPage from "./components/quiz/QuizPage";
 
 //JY
 import SkeletonFunc from "./components/test/SkeletonFunc";
@@ -71,7 +75,11 @@ function App() {
             <Route path="/introduce" element={<Introduce />} />
             <Route path="/mypage" element={<Mypage />} />
             <Route path="/bookmark" element={<Bookmark />} />
+            <Route path="/like" element={<Like />} />
             <Route path="/top10" element={<TopTenSOTB />} />
+            <Route path="/userinfo" element={<UserInfo />} />
+            <Route path="/usertab" element={<UserTab />} />
+            <Route path="/quiz" element={<QuizPage />} />
           </Routes>
         </Router>
       </UserStateContext.Provider>
