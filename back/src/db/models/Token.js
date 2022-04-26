d16229055e13d8678b0ede2a31d864ea92ea230a
parkjungyoon <tbr06057@naver.com>
2022-04-26 13:17:37 +0900

    [Add] fubdToken

--- back/src/db/models/Token.js
+++ back/src/db/models/Token.js
@@ -1,6 +1,11 @@
 import { Token } from "../schemas/token";
 
 class TokenModel {
+  static async findToken(userId) {
+    const userToken = await Token.findOne({ _id: userId });
+    return userToken;
+  }
+
   static updateRefresh = async ({ _id, refreshToken }) => {
     const update = await Token.updateOne(
       { _id },
