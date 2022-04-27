911b60a6304bcd39c71a49d672e721d1ed5028a7
JinKris <vain65@naver.com>
2022-04-27 12:24:18 +0900

    [Modigy] modify loginForm handleSubmit

--- front/src/components/user/LoginForm.js
+++ front/src/components/user/LoginForm.js
@@ -52,12 +52,18 @@ function LoginForm() {
         email,
         password,
       });
+      //유저 정보 확인용 (test)
+      console.log(`console log res : ${res}`);
       // 유저 정보는 response의 data임.
       const user = res.data;
       // JWT 토큰은 유저 정보의 token임.
       const jwtToken = user.token;
       // sessionStorage에 "userToken"이라는 키로 JWT 토큰을 저장함.
       sessionStorage.setItem("userToken", jwtToken);
+
+      /* sessionStorage에 'userRefreshToken'이라는 키로 JWT 토큰을 저장함..?
+      sessionStorage.setItem('userRefreshToken',)*/
+
       // dispatch 함수를 이용해 로그인 성공 상태로 만듦.
       userDispatch({
         type: "LOGIN_SUCCESS",
