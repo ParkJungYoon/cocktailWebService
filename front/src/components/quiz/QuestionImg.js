7e6b8c3804336dea93fd77586d0e993cc60b9076
ksm9317 <ksm9317@gmail.com>
2022-04-28 15:37:45 +0900

    [Modify] 코드 수정

--- front/src/components/quiz/QuestionImg.js
+++ front/src/components/quiz/QuestionImg.js
@@ -9,11 +9,9 @@ const QuestionImg = (props) => {
           sx={{
             width: "100%",
             height: "100%",
-            display: "flex",
             justifyContent: "center",
           }}
           backgroundColor="transparent"
-          display="flex"
         >
           <CardMedia
             component="img"
