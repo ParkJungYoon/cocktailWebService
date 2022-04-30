42b5eeadd5958a39857eed4d046184816eeccbf1
JinKris <vain65@naver.com>
2022-04-30 16:15:15 +0900

    [Add] test-wordCloud chart

--- front/src/components/landing/Thirdpage.js
+++ front/src/components/landing/Thirdpage.js
@@ -1,8 +1,22 @@
-import * as React from "react";
+import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Button, Grid, Typography } from "@mui/material";
+import WordCloud from "./WordCloud";
 
 export default function Thirdpage() {
+  // const wordCloudStart = useRef();
+  // WordCloud(wordCloudStart, {
+  //   list: [
+  //     ["first", 1],
+  //     ["second", 2],
+  //     ["third", 3],
+  //     ["forth", 4],
+  //     ["five", 5],
+  //     ["six", 6],
+  //     ["seven", 7],
+  //   ],
+  // });
+
   const navigate = useNavigate();
   return (
     <div className="thirdpageContainer">
@@ -11,6 +25,7 @@ export default function Thirdpage() {
           <Typography className="dictionaryText" variant="h4">
             Or You can search You can search on Our Cocktail Dictionary
           </Typography>
+          {/* <canvas ref={wordCloudStart}></canvas> */} {/* (test-ing) */}
           <Box className="buttonBox">
             <Button
               variant="contained"
