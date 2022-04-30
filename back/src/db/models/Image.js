be6d86c61eeb2c7091e9f6bd31192c18b670c798
parkjungyoon <tbr06057@naver.com>
2022-04-30 21:00:28 +0900

    [Fix] merge

--- back/src/db/models/Image.js
+++ back/src/db/models/Image.js
@@ -1,50 +1,52 @@
 import { Image } from "../schemas/image";
-import path from 'path';
-import multer from 'multer';
+import path from "path";
+import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
 
-const imagePath = () => { return path.resolve(__dirname, '../', '../', '../', 'images') }
+const imagePath = () => {
+  return path.resolve(__dirname, "../", "../", "../", "images");
+};
 
 const imageNamePush = (req, filename) => {
   if (req.images == undefined) {
-    req.images = []
+    req.images = [];
   }
 
-  req.images.push(filename)
+  req.images.push(filename);
   return req;
-}
+};
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, imagePath());
-    },
-    filename: function (req, file, cb) {
-      const fileName = new Date().valueOf() + '_' + uuidv4() + '_' + file.originalname
-      
-      const image = {
-          fileName : fileName,
-          imageUrl : path.join(imagePath(), fileName),
-          owner : req.user,
-          type : path.extname(file.originalname)
-      }
+  destination: function (req, file, cb) {
+    cb(null, imagePath());
+  },
+  filename: function (req, file, cb) {
+    const fileName =
+      new Date().valueOf() + "_" + uuidv4() + "_" + file.originalname;
 
-      ImageModel.uploadOne({ image });
-      req = imageNamePush(req, fileName)
-      cb(null, fileName);
-    },
+    const image = {
+      fileName: fileName,
+      imageUrl: path.join(imagePath(), fileName),
+      owner: req.user,
+      type: path.extname(file.originalname),
+    };
+
+    ImageModel.uploadOne({ image });
+    req = imageNamePush(req, fileName);
+    cb(null, fileName);
+  },
 });
-  
 
 class ImageModel {
   /**
-   * @param {이미지 정보} image 
+   * @param {이미지 정보} image
    * @return true or false
    */
   static uploadOne = async ({ image }) => {
-    const result = Image.create(image)
+    const result = Image.create(image);
     if (result != null) {
-        return true;
+      return true;
     }
     return false;
   };
@@ -54,9 +56,9 @@ class ImageModel {
    * @return true or false
    */
   static deleteByUser = async ({ owner }) => {
-    const result = Image.deleteMany({ owner : owner })
-    if (result.deletedCount > 0) { 
-        return true
+    const result = Image.deleteMany({ owner: owner });
+    if (result.deletedCount > 0) {
+      return true;
     }
     return false; //삭제된 이미지가 없으면
   };
@@ -66,10 +68,10 @@ class ImageModel {
    * @return true or false
    */
   static deleteOne = async ({ fileName }) => {
-    const result = Image.deleteOne({ fileName : fileName })
-    
-    if (result.deletedCount > 0) { 
-        return true
+    const result = Image.deleteOne({ fileName: fileName });
+
+    if (result.deletedCount > 0) {
+      return true;
     }
     return false; //삭제된 이미지가 없으면
   };
@@ -82,18 +84,17 @@ class ImageModel {
   static getImgOne = async ({ fileName }) => {
     const image = await Image.findOne({ fileName });
 
-    if (image == null){
+    if (image == null) {
       return null;
     }
 
-    const file = fs.readFileSync(image.imageUrl)
+    const file = fs.readFileSync(image.imageUrl);
 
-    let fileJson = file.toJSON()
-    fileJson['type'] = image.type
-    
+    let fileJson = file.toJSON();
+    fileJson["type"] = image.type;
+
     return fileJson;
   };
-
 
   /**
    * @param {이미지 파일 이름 리스트} fileNameList
@@ -101,16 +102,16 @@ class ImageModel {
    * @return images => [ {type:.jpg, data:~~~~}, {...} ]
    */
   static getImg = async ({ fileNameList }) => {
-    let imgList = []
+    let imgList = [];
 
-    for(let i = 0; i < fileNameList.length; i++) {
+    for (let i = 0; i < fileNameList.length; i++) {
       const result = await this.getImgOne({ fileName: fileNameList[i] });
       if (result !== null) {
-        imgList.push(result)
+        imgList.push(result);
       }
     }
-    
-    return imgList
+
+    return imgList;
   };
 
   /**
@@ -118,11 +119,7 @@ class ImageModel {
    * @param {프론트 => Response} res
    * @return send images
    */
-  static getImgByUser = async (res, { owner }) => {
-
-  };
-
-
+  static getImgByUser = async (res, { owner }) => {};
 }
 
 export { ImageModel, storage };
