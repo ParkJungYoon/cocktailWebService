e989a80208c6adbd2e31cd2b13debde5370d9a47
ksm9317 <ksm9317@gmail.com>
2022-05-03 01:55:51 +0900

    [Add] mbti type data

---
+++ front/src/components/mbti/TypeData.js
@@ -0,0 +1,42 @@
+import Mojito from "../../imgs/mojito.jpg";
+
+const checkState = {
+  types: {
+    ISTJ: "Mojito",
+    ISTP: "Mojito",
+    ISFJ: "Mojito",
+    ISFP: "Mojito",
+    INTJ: "Mojito",
+    INTP: "Mojito",
+    INFJ: "Mojito",
+    INFP: "Mojito",
+    ESTJ: "Mojito",
+    ESTP: "Mojito",
+    ESFJ: "Mojito",
+    ESFP: "Mojito",
+    ENTJ: "Mojito",
+    ENTP: "Mojito",
+    ENFJ: "Mojito",
+    ENFP: "Mojito",
+  },
+
+  typeImgs: {
+    ISTJ: Mojito,
+    ISTP: Mojito,
+    ISFJ: Mojito,
+    ISFP: Mojito,
+    INTJ: Mojito,
+    INTP: Mojito,
+    INFJ: Mojito,
+    INFP: Mojito,
+    ESTJ: Mojito,
+    ESTP: Mojito,
+    ESFJ: Mojito,
+    ESFP: Mojito,
+    ENTJ: Mojito,
+    ENTP: Mojito,
+    ENFJ: Mojito,
+    ENFP: Mojito,
+  },
+};
+export default checkState;
