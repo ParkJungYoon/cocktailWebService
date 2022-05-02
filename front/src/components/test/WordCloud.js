05873e8626700d221e06ea5d7a9887248c8e1f5f
JinKris <vain65@naver.com>
2022-05-02 13:48:35 +0900

    [Modify] modify wordCloud

--- front/src/components/test/WordCloud.js
+++ front/src/components/test/WordCloud.js
@@ -5,7 +5,7 @@ import ReactWordcloud from "react-wordcloud";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/animations/scale.css";
 
-import words from "./words";
+// import words from "./words";
 import * as Api from "../../api";
 
 const options = {
@@ -26,9 +26,125 @@ const options = {
 
 function WordCloud() {
   useEffect(async () => {
-    const res = await Api.get("userLike");
+    const res = await Api.get("cocktails/like");
     console.log(res.data);
   }, []);
+
+  const words = [
+    {
+      name: "told",
+      likes: 64,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {},
+    {
+      text: "best",
+      value: 54,
+    },
+    {
+      text: "mouth",
+      value: 20,
+    },
+    {
+      name: "staff",
+      value: 64,
+    },
+    {
+      text: "gum",
+      value: 10,
+    },
+    {
+      text: "chair",
+      value: 12,
+    },
+    {
+      text: "ray",
+      value: 22,
+    },
+    {
+      text: "dentistry",
+      value: 11,
+    },
+    {
+      text: "canal",
+      value: 13,
+    },
+    {
+      text: "procedure",
+      value: 32,
+    },
+    {
+      text: "filling",
+      value: 26,
+    },
+    {
+      text: "gentle",
+      value: 19,
+    },
+    {
+      text: "cavity",
+      value: 17,
+    },
+    {
+      text: "crown",
+      value: 14,
+    },
+    {
+      text: "cleaning",
+      value: 38,
+    },
+    {
+      text: "hygienist",
+      value: 24,
+    },
+    {
+      text: "dental",
+      value: 59,
+    },
+    {
+      text: "charge",
+      value: 24,
+    },
+    {
+      text: "cost",
+      value: 29,
+    },
+    {
+      text: "charged",
+      value: 13,
+    },
+    {
+      text: "spent",
+      value: 17,
+    },
+    {
+      text: "paying",
+      value: 14,
+    },
+    {
+      text: "pocket",
+      value: 12,
+    },
+    {
+      text: "dollar",
+      value: 11,
+    },
+    {
+      text: "business",
+      value: 32,
+    },
+    {
+      text: "refund",
+      value: 10,
+    },
+  ];
   return (
     <div>
       <div style={{ height: 400, width: 600 }}>
