afddc84a4f2868dec226dd61236db8d3c622846b
JinKris <vain65@naver.com>
2022-05-07 02:46:07 +0900

    [Add] add snakbar provider - notistack

--- front/src/index.js
+++ front/src/index.js
@@ -3,13 +3,15 @@ import React from "react";
 import App from "./App";
 
 import { UserProvider } from "./components/user/reducer/userReducer";
+import { SnackbarProvider } from "notistack";
 // import useUserHook as UserHook from "./components/commons/useUserHook";
 
 ReactDOM.render(
   <React.StrictMode>
     <UserProvider>
-      <App />
-      {/* <useUserHook /> */}
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
     </UserProvider>
   </React.StrictMode>,
   document.getElementById("root")
